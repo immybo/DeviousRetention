921b697947bac7d5335b68657a64fb638ba52aaa
R. Campbell <robertcampbellnz@gmail.com>
2018-01-24 18:38:14 +1300

    make a reasonable world

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -26,33 +26,58 @@ public class Main {
     public static void main(String[] args) {
         currentPlayerNumber = 0;
 
-        Tile[][] tiles = new Tile[10][10];
+        Tile[][] tiles = new Tile[30][30];
         for (int i = 0; i < tiles.length; i++) {
             for (int j = 0; j < tiles[0].length; j++) {
                 tiles[i][j] = new GrassTile();
             }
         }
-        tiles[5][5] = new MountainTile();
+
+        for (int i = 12; i < 19; i++) {
+            for (int j = 12; j < 19; j++) {
+                tiles[i][j] = new MountainTile();
+            }
+        }
+
         Board board = new Board(tiles);
 
-        UnitTemplate testUnit = new UnitTemplate("TestUnit", new Entity.Ability[]{Entity.Ability.ATTACK, Entity.Ability.GATHER}, 1, 2, 10, 50, 0.1, 100, new Cost(100));
-        BuildingTemplate testBuilding = new BuildingTemplate("TestBuilding", new Entity.Ability[]{}, 5, 5, 50, 2, 500, new Cost(300), new UnitTemplate[]{testUnit, testUnit}, new int[]{10});
-        ResourceTemplate testResource = new ResourceTemplate("TestResource", 0.5, 1000, 1);
+        UnitTemplate gatherer = new UnitTemplate("Gatherer", new Entity.Ability[]{Entity.Ability.ATTACK, Entity.Ability.GATHER}, 0.2, 1, 10, 10, 0.2, 100, new Cost(150));
+        UnitTemplate archer = new UnitTemplate("Archer", new Entity.Ability[]{Entity.Ability.ATTACK}, 1, 6, 5, 20, 0.15, 100, new Cost(200));
+        UnitTemplate infantry = new UnitTemplate("Infantry", new Entity.Ability[]{Entity.Ability.ATTACK}, 0.3, 1, 10, 75, 0.3, 200, new Cost(200));
+        UnitTemplate cavalry = new UnitTemplate("Cavalry", new Entity.Ability[]{Entity.Ability.ATTACK}, 1.5, 1, 10, 75, 0.5, 350, new Cost(600));
+
+        BuildingTemplate headquarters = new BuildingTemplate("Headquarters", new Entity.Ability[]{}, 0, 1, 0, 1.5, 5000, new Cost(5000), new UnitTemplate[]{gatherer}, new int[]{10});
+        BuildingTemplate barracks = new BuildingTemplate("Barracks", new Entity.Ability[]{}, 0, 1, 0, 1, 2500, new Cost(2000), new UnitTemplate[]{archer, infantry, cavalry}, new int[]{20, 20, 50});
+
+        ResourceTemplate bigResource = new ResourceTemplate("Large Gold Mine", 2, 10000, 1);
+        ResourceTemplate smallResource = new ResourceTemplate("Small Gold Mine", 1, 1500, 1.5);
 
         World world = new World(board);
-        world.addEntity(testUnit.create(world, 0, 0.5, 0.5));
-        world.addEntity(testUnit.create(world, 1, 1.5, 1.5));
-        world.addEntity(testBuilding.create(world, 1, 3, 3));
-        world.addEntity(testResource.create(world, -1, 4.5, 4.5));
+
+        int bottom = world.getBoard().getHeight();
+        int right = world.getBoard().getWidth();
+
+        world.addEntity(headquarters.create(world, 0, 4, 4));
+        world.addEntity(gatherer.create(world, 0, 4, 6));
+        world.addEntity(gatherer.create(world, 0, 4, 7));
+
+        world.addEntity(headquarters.create(world, 1, right - 4, bottom - 4));
+        world.addEntity(gatherer.create(world, 1, right - 4, bottom - 6));
+        world.addEntity(gatherer.create(world, 1, right - 4, bottom - 7));
+
+        world.addEntity(bigResource.create(world, -1, 10, 10));
+        world.addEntity(bigResource.create(world, -1, 20, 20));
+        world.addEntity(smallResource.create(world, -1, 20, 10));
+        world.addEntity(smallResource.create(world, -1, 10, 20));
 
         Server server = new Server(world);
-        launchClient(server, new BuildingTemplate[]{testBuilding});
+        launchClient(server, new BuildingTemplate[]{headquarters, barracks});
         try {
             Thread.sleep(500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        launchClient(server, new BuildingTemplate[]{testBuilding});
+        launchClient(server, new BuildingTemplate[]{headquarters, barracks});
 
         try {
             Thread.sleep(500);
